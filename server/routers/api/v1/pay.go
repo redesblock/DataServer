2ead1c78e0458d948a80c7661f4ffb2a8f185156
sidtt <sidtt@users.noreply.github.com>
2023-05-29 11:40:39 +0800

    f

--- server/routers/api/v1/pay.go
+++ server/routers/api/v1/pay.go
@@ -1,11 +1,13 @@
 package v1
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-pay/gopay"
+	"github.com/go-pay/gopay/wechat/v3"
 	"github.com/redesblock/dataserver/models"
 	"github.com/redesblock/dataserver/server/pay"
+	"github.com/shopspring/decimal"
 	"github.com/smartwalle/alipay/v3"
 	"gorm.io/gorm"
 	"net/http"
@@ -41,9 +43,7 @@ func AlipayNotify(db *gorm.DB) func(c *gin.Context) {
 			c.JSON(OKCode, NewResponse(c, ExecuteCode, err))
 			return
 		}
-		bts, _ := json.Marshal(noti)
-		fmt.Println("notfiy", string(bts))
-		_ = noti
+
 		var order models.Order
 		ret := db.Model(&models.Order{}).Where("order_id = ?", noti.OutTradeNo).Find(&order)
 		if err := ret.Error; err != nil {
@@ -100,6 +100,70 @@ func AlipayNotify(db *gorm.DB) func(c *gin.Context) {
 
 func WxPayNotify(db *gorm.DB) func(c *gin.Context) {
 	return func(c *gin.Context) {
+		notifyReq, err := wechat.V3ParseNotify(c.Request)
+		if err != nil {
+			c.JSON(OKCode, NewResponse(c, ExecuteCode, err))
+			return
+		}
 
+		// 获取微信平台证书
+		certMap := pay.WXClient.WxPublicKeyMap()
+		// 验证异步通知的签名
+		err = notifyReq.VerifySignByPKMap(certMap)
+		if err != nil {
+			c.JSON(OKCode, NewResponse(c, ExecuteCode, err))
+			return
+		}
+
+		noti, err := notifyReq.DecryptCipherText(string(pay.WXClient.ApiV3Key))
+		if err != nil {
+			c.JSON(OKCode, NewResponse(c, ExecuteCode, err))
+			return
+		}
+		var order models.Order
+		ret := db.Model(&models.Order{}).Where("order_id = ?", noti.OutTradeNo).Find(&order)
+		if err := ret.Error; err != nil {
+			c.JSON(OKCode, NewResponse(c, ExecuteCode, err))
+			return
+		}
+
+		switch notifyReq.EventType {
+		case wechat.TradeStateClosed:
+			if order.Status == models.OrderCancel {
+				order.Status = models.OrderCancel
+				if err := db.Save(&order).Error; err != nil {
+					c.JSON(OKCode, NewResponse(c, ExecuteCode, err))
+					return
+				}
+			}
+		case wechat.TradeStateNoPay:
+
+		case wechat.TradeStateSuccess:
+			if order.Status == models.OrderSuccess {
+				order.Status = models.OrderSuccess
+				order.PaymentID = noti.TransactionId
+				order.PaymentAccount = noti.Payer.Openid
+				order.ReceiveAccount = decimal.NewFromInt(int64(noti.Amount.PayerTotal)).Div(decimal.NewFromInt(10)).String()
+				order.PaymentAmount = decimal.NewFromInt(int64(noti.Amount.PayerTotal)).Div(decimal.NewFromInt(10)).String()
+				order.PaymentTime, _ = time.Parse(models.TIME_FORMAT, noti.SuccessTime)
+				if err := db.Transaction(func(tx *gorm.DB) error {
+					var user models.User
+					if ret := tx.Model(&models.User{}).Where("id = ?", order.UserID).Find(&user); ret.Error != nil {
+						return ret.Error
+					} else if ret.RowsAffected == 0 {
+						return fmt.Errorf("not found user")
+					}
+					user.TotalStorage += order.Quantity
+					if err := tx.Save(&user).Error; err != nil {
+						return err
+					}
+					return tx.Save(&order).Error
+				}); err != nil {
+					c.JSON(OKCode, NewResponse(c, ExecuteCode, err))
+					return
+				}
+			}
+		}
+		c.JSON(http.StatusOK, &wechat.V3NotifyRsp{Code: gopay.SUCCESS, Message: "成功"})
 	}
 }
